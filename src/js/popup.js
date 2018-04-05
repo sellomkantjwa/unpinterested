e62a85190a4c67706581493f019957fa42f85153
Sello <giza182@gmail.com>
2018-04-05 12:35:03 +0200

    initiaise description correctly

--- src/js/popup.js
+++ src/js/popup.js
@@ -6,18 +6,22 @@ let descriptionDiv = document.getElementById("description");
 
 chrome.storage.sync.get("isDisabled", function (data) {
   toggleOnOffBtn.checked = !data.isDisabled;
+  setDescriptionText(data.isDisabled);
+
 })
 
 
 toggleOnOffBtn.onclick = function toggleOnOff(btn) {
   let isDisabled = !document.getElementById('toggleOnOffBtn').checked;
+  setDescriptionText(isDisabled);
 
+  chrome.storage.sync.set({isDisabled: isDisabled})
+};
+
+function setDescriptionText(isDisabled) {
   if (isDisabled) {
     descriptionDiv.innerHTML = "Not excluding pinterest results";
   } else {
     descriptionDiv.innerHTML = "Excluding pinterest results";
   }
-
-  chrome.storage.sync.set({isDisabled: isDisabled})
-};
-
+}
